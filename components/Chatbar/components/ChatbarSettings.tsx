86c7c8497d86bf97030924c7e6178058ac553f73
Per Størup Lauridsen <per@iderummet.dk>
2023-07-22 18:33:18 +0200

    Logud

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -35,7 +35,14 @@ export const ChatbarSettings = () => {
     handleExportData,
     handleApiKeyChange,
   } = useContext(ChatbarContext);
-
+  
+  const handleLogout = () => {
+    // Sæt isLoggedIn-tilstanden til false
+    setIsLoggedIn(false);
+  
+    // Fjern brugerens login-status fra local storage
+    localStorage.removeItem('isLoggedIn');
+  };
   return (
     <div className="flex flex-col items-center space-y-1 border-t border-white/20 pt-1 text-sm">
       {conversations.length > 0 ? (
@@ -50,24 +57,31 @@ export const ChatbarSettings = () => {
         onClick={() => handleExportData()}
       />
 
+
       <SidebarButton
         text={t('Settings')}
         icon={<IconSettings size={18} />}
-        onClick={() => setIsSettingDialog(true)}
+        onClick={handleLogout}
       />
 
-      {!serverSideApiKeyIsSet ? (
-        <Key apiKey={apiKey} onApiKeyChange={handleApiKeyChange} />
-      ) : null}
+     // <SidebarButton
+     //   text={t('Settings')}
+     //   icon={<IconSettings size={18} />}
+     //   onClick={() => setIsSettingDialog(true)}
+     // />
 
-      {!serverSidePluginKeysSet ? <PluginKeys /> : null}
+     // {!serverSideApiKeyIsSet ? (
+      //  <Key apiKey={apiKey} onApiKeyChange={handleApiKeyChange} />
+     // ) : null}
 
-      <SettingDialog
-        open={isSettingDialogOpen}
-        onClose={() => {
-          setIsSettingDialog(false);
-        }}
-      />
+     //  {!serverSidePluginKeysSet ? <PluginKeys /> : null}
+    
+     // <SettingDialog
+     //   open={isSettingDialogOpen}
+     //   onClose={() => {
+     //     setIsSettingDialog(false);
+     //   }}
+     // />
     </div>
   );
 };
