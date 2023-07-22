cde94a1112316fd88cb1a694b88ca8ce4e940921
Per Størup Lauridsen <per@iderummet.dk>
2023-07-22 18:00:59 +0200

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -353,7 +353,7 @@ const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     if (localStorage.getItem('isLoggedIn') === 'true') {
       setIsLoggedIn(true);
     }
-  }, [setIsLoggedIn]);
+  }, []);
 
   
   
@@ -400,46 +400,6 @@ const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
   return (
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
       {!isLoggedIn ? (
-
-<div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
-          <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
-          </div>
-          <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-            <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
-            </div>
-          </div>
-          <div className="text-center text-gray-500 dark:text-gray-400">
-            <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
-            </div>
-            <div className="mb-2">
-              It is <span className="italic">only</span> used to communicate
-              with their API.
-            </div>
-            <div className="mb-2">
-              {t(
-                'Please set your OpenAI API key in the bottom left of the sidebar.',
-              )}
-            </div>
-            <div>
-              {t("If you don't have an OpenAI API key, you can get one here: ")}
-              <a
-                href="https://platform.openai.com/account/api-keys"
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                openai.com
-              </a>
-            </div>
-          </div>
-        </div>
-
-
         <div className="flex items-center justify-center h-full">
           <form onSubmit={handleLogin} className="flex flex-col items-center">
   <h1>Your Heading</h1>
@@ -448,7 +408,7 @@ const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     type="text"
     value={password}
     onChange={(e) => setPassword(e.target.value)}
-    class={{
+    style={{
       borderRadius: "15px",
       padding: "10px 15px",
       fontSize: "1.2em",
@@ -471,19 +431,10 @@ const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
   >
     Log in
   </button>
-
-
-
-
-
-
-
   {loginError && <div className="error">{loginError}</div>}
 </form>
         </div>
       ) : !(apiKey || serverSideApiKeyIsSet) ? (
-
-
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
             Welcome to Chatbot UI
@@ -521,9 +472,6 @@ const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
             </div>
           </div>
         </div>
-
-
-
       ) : modelError ? (
         <ErrorMessageDiv error={modelError} />
       ) : (
